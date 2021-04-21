407b98050cb83515cd4a73e3803a4dbecf25a94f
Chris Abiva <chrisabiva@hotmail.com>
2021-04-20 23:16:20 -0700

    Adding changes on the css and script

--- js/script.js
+++ js/script.js
@@ -5,4 +5,9 @@ var today = moment();
 var TodaysDate = today.format("dddd, MMMM Do");
 $("#currentDay").text(TodaysDate);
 
-var 
+// Starts with V ends with ARIABLE
+var tr9El = document.querySelector("#tr9");
+var tr9txtEl = document.getElementById("9AM");
+
+let source = document.getElementById("currentDay")
+tr9txtEl.value = source.innerText;
