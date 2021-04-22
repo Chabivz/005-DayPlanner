a6703e5fdacc684f0f5755da66a16a17cb2ea2dc
Chris Abiva <chrisabiva@hotmail.com>
2021-04-21 23:17:20 -0700

    Adding more changes to the JS, CSS and HTML

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
 
 // Header Date
 
-var today = moment();
-var TodaysDate = today.format("dddd, MMMM Do");
+let today = moment();
+let TodaysDate = today.format("dddd, MMMM Do");
 $("#currentDay").text(TodaysDate);
 
 // Starts with V ends with ARIABLE
-var tr9El = document.querySelector("#tr9");
-var tr9txtEl = document.getElementById("9AM");
+let tr9El = document.querySelector("#tr9");
+let tr9txtEl = document.getElementById("9AM");
+let plansEl = $('.plans');
+let source = document.getElementById("currentDay");
+let clockValEl = $('.clockValidation');
+// 
+// jQuery disable text area
+plansEl.prop('disabled', true);
 
-let source = document.getElementById("currentDay")
-tr9txtEl.value = source.innerText;
+// tr9txtEl.value = source.innerText; 
+
+function updatePlans(plans) {
+    if ( plansEl.prop('disabled') == true) {
+    plansEl.prop('disabled', false);
+    console.log("set value to false");
+
+} 
+
+// save the text when insert stuff on the text box 
+function save(saveBtn) {
+    let schedulerStorage = JSON(localStorage.getItem("schedulerStorage")) || [];
+
+}
+
+
+// checking the hour clock if the
+function clockChecker() {
+    for ( let i = 0 ; clockValEl <= i ; i++ ) {
+
+        
+
+    }
+
+}
